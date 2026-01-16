9c07b7b244c5ee9373fa9b33b28676c0fae0613c
jinchangsung <ai.jinpd@gmail.com>
2026-01-17 02:07:26 +0900

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
+    // Important: base: './' allows the app to run in subdirectories (like GitHub Pages)
+    base: './',
     define: {
       // Polyfill process.env.API_KEY for the app usage
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
